4ae1d6620258637e07a78e5d73becb15306ba495
Hannah Long <geraldlong@wl-221-215.wpa.claremont.edu>
2014-09-09 16:13:59 -0700

    Initial Commit

---
+++ ViewTest/HLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HLAppDelegate.h
+//  ViewTest
+//
+//  Created by Hannah Long on 9/9/14.
+//  Copyright (c) 2014 Hannah Long. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
