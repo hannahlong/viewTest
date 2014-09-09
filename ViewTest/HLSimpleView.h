b58448a01e10632c03e2ae4e87652e1422fcd3cc
Hannah Long <geraldlong@wl-221-215.wpa.claremont.edu>
2014-09-09 16:39:10 -0700

    viewTest

---
+++ ViewTest/HLSimpleView.h
@@ -0,0 +1,13 @@
+//
+//  HLSimpleView.h
+//  ViewTest
+//
+//  Created by Hannah Long on 9/9/14.
+//  Copyright (c) 2014 Hannah Long. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HLSimpleView : UIView
+
+@end
---
+++ ViewTest/HLSimpleView.m
@@ -0,0 +1,13 @@
+//
+//  HLSimpleView.m
+//  ViewTest
+//
+//  Created by Hannah Long on 9/9/14.
+//  Copyright (c) 2014 Hannah Long. All rights reserved.
+//
+
+#import "HLSimpleView.h"
+
+@implementation HLSimpleView
+
+@end
